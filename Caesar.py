5276fc88c4d73c97c37151014995a47e1e2bf42f
nyg1 <46832635+nyg1@users.noreply.github.com>
2019-11-09 16:04:33 -0700

    Update Caesar.py

--- Caesar.py
+++ Caesar.py
@@ -1,20 +1,22 @@
 # This is a Caesar Cipher
+def encrypt(text):
+    import numpy
+    word = input("Enter your code: ")
+    word = word.lower()
+    shift = input("Enter shift number: ")
+    shift = int(shift)
+    length = len(word)
+    encrypted_num =[]
+    for n in range(length):
+        a = word[n]
+        encrypted_num.append(ord(a) - 97)
 
-import numpy
-word = input("Enter your code: ")
-word = word.lower()
-shift = input("Enter shift number: ")
-shift = int(shift)
-length = len(word)
-encrypted_num =[]
-for n in range(length):
-    a = word[n]
-    encrypted_num.append(ord(a) - 97)
+    encrypted_nums = [x+shift for x in encrypted_num]
 
-encrypted_nums = [x+shift for x in encrypted_num]
+    for i in range(length):
+         encrypted_nums[i]=encrypted_nums[i]%26 + 97
 
-for i in range(length):
-     encrypted_nums[i]=encrypted_nums[i]%26 + 97
+    encrypted_letters = ''.join(chr(i) for i in encrypted_nums)
+    print(encrypted_letters)
 
-encrypted_letters = ''.join(chr(i) for i in encrypted_nums)
-print(encrypted_letters)
+def decrypt(text):
