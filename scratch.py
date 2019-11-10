314e6f06d2f519b83c345dbca74c00884931d96b
tig3r66 <eguo1@ualberta.ca>
2019-11-10 08:41:04 -0700

    better formatted

--- scratch.py
+++ scratch.py
@@ -1,62 +1,62 @@
-import tkinter
+import tkinter as tk
 
-root = tkinter.Tk()
+root = tk.Tk()
 root.title("Encryptodoodles")
-frame = tkinter.Frame(root)
+frame = tk.Frame(root)
 frame.pack()
 
-topframe = tkinter.Frame(root)
-topframe.pack(side=tkinter.TOP)
-bottomframe = tkinter.Frame(root)
-bottomframe.pack(side=tkinter.BOTTOM)
+topframe = tk.Frame(root)
+topframe.pack(side=tk.TOP)
+bottomframe = tk.Frame(root)
+bottomframe.pack(side=tk.BOTTOM)
 
 def get_text_type():
-    label = tkinter.Label(topframe,
+    label = tk.Label(topframe,
         text='Text Type\nOption 1: use .txt file\n'
         'Option 2: enter a message to encrypt or decrypt\n'
         'Enter 1 or 2: ')
     label.pack()
 
-    button1 = tkinter.Button(root, text = "1", width = 10, height = 2)
-    button1.pack(side = tkinter.TOP)
-    button2 = tkinter.Button(root, text = "2", width = 10, height = 2)
-    button2.pack(side = tkinter.TOP)
+    button1 = tk.Button(root, text = "1", width = 10, height = 2)
+    button1.pack(side = tk.TOP)
+    button2 = tk.Button(root, text = "2", width = 10, height = 2)
+    button2.pack(side = tk.TOP)
 
 
 def get_cypher_type():
-    label = tkinter.Label(topframe, text='Option 1: encryption\nOption 2: decryption\n'
+    label = tk.Label(topframe, text='Option 1: encryption\nOption 2: decryption\n'
                        'Enter 1 or 2: ')
     label.pack()
 
-    button1 = tkinter.Button(root, text = "1", width = 10, height = 2)
-    button1.pack(side = tkinter.TOP)
-    button2 = tkinter.Button(root, text = "2", width = 10, height = 2)
-    button2.pack(side = tkinter.TOP)
+    button1 = tk.Button(root, text = "1", width = 10, height = 2)
+    button1.pack(side = tk.TOP)
+    button2 = tk.Button(root, text = "2", width = 10, height = 2)
+    button2.pack(side = tk.TOP)
 
 
 def get_encryption_type():
-    label = tkinter.Label(topframe, text='Cipher Options')
+    label = tk.Label(topframe, text='Cipher Options')
     label.pack()
 
-    button1 = tkinter.Button(root, text = "1: Caesar", width = 10, height = 2)
-    button1.pack(side = tkinter.TOP)
-    button2 = tkinter.Button(root, text = "2: Vigenere", width = 10, height = 2)
-    button2.pack(side = tkinter.TOP)
-    button3 = tkinter.Button(root, text = "3: Enigma", width = 10, height = 2)
-    button3.pack(side = tkinter.TOP)
-    button4 = tkinter.Button(root, text = "4: AES", width = 10, height = 2)
-    button4.pack(side = tkinter.TOP)
-    button5 = tkinter.Button(root, text = "5: Playfair", width = 10, height = 2)
-    button5.pack(side = tkinter.TOP)
+    button1 = tk.Button(root, text = "1: Caesar", width = 10, height = 2)
+    button1.pack(side = tk.TOP)
+    button2 = tk.Button(root, text = "2: Vigenere", width = 10, height = 2)
+    button2.pack(side = tk.TOP)
+    button3 = tk.Button(root, text = "3: Enigma", width = 10, height = 2)
+    button3.pack(side = tk.TOP)
+    button4 = tk.Button(root, text = "4: AES", width = 10, height = 2)
+    button4.pack(side = tk.TOP)
+    button5 = tk.Button(root, text = "5: Playfair", width = 10, height = 2)
+    button5.pack(side = tk.TOP)
 
 
 def cont():
-    label = tkinter.Label(topframe, text='Continue?')
+    label = tk.Label(topframe, text='Continue?')
     label.pack()
 
-    button1 = tkinter.Button(root, text = "Yes", width = 5, height = 2)
+    button1 = tk.Button(root, text = "Yes", width = 5, height = 2)
     button1.pack()
-    button2 = tkinter.Button(root, text = "No", width = 5, height = 2)
+    button2 = tk.Button(root, text = "No", width = 5, height = 2)
     button2.pack()
 
 
