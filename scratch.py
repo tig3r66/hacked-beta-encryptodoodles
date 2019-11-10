78abb73e656554afd8d18a98326475d48a3df0b0
tig3r66 <eguo1@ualberta.ca>
2019-11-10 08:38:57 -0700

    buttons for the GUI

--- scratch.py
+++ scratch.py
@@ -1,88 +1,68 @@
-import pyperclip
-import Enigma
-import Caesar
-import Readtxt
-import Vigenere
-import playfair
-import importlib
-# import AES
+import tkinter
 
+root = tkinter.Tk()
+root.title("Encryptodoodles")
+frame = tkinter.Frame(root)
+frame.pack()
+
+topframe = tkinter.Frame(root)
+topframe.pack(side=tkinter.TOP)
+bottomframe = tkinter.Frame(root)
+bottomframe.pack(side=tkinter.BOTTOM)
 
 def get_text_type():
-    text_type = input('\nOption 1: you have a .txt file to encrypt/decrypt\n'
-        'Option 2: you wish to enter a message to encrypt/decrypt\n'
+    label = tkinter.Label(topframe,
+        text='Text Type\nOption 1: use .txt file\n'
+        'Option 2: enter a message to encrypt or decrypt\n'
         'Enter 1 or 2: ')
-    while (text_type.isdigit() is False) or (int(text_type) < 0) or (int(text_type) > 5):
-        print("\nInvalid selection. Please try again.")
-        text_type = input('Option 1: you have a .txt file to encrypt/decrypt\n'
-        'Option 2: you wish to enter a message to encrypt/decrypt\n'
-        'Enter 1 or 2: ')
-    return int(text_type)
+    label.pack()
+
+    button1 = tkinter.Button(root, text = "1", width = 10, height = 2)
+    button1.pack(side = tkinter.TOP)
+    button2 = tkinter.Button(root, text = "2", width = 10, height = 2)
+    button2.pack(side = tkinter.TOP)
 
 
 def get_cypher_type():
-    CypherType = input('Option 1: encryption\nOption 2: decryption\n'
+    label = tkinter.Label(topframe, text='Option 1: encryption\nOption 2: decryption\n'
                        'Enter 1 or 2: ')
-    while (CypherType.isdigit() is False) or (int(CypherType) < 0) or (int(CypherType) > 5):
-        print("\nInvalid selection. Please try again.")
-        CypherType = input('Option 1: encryption\nOption 2: decryption\n'
-                           'Enter 1 or 2: ')
-    return int(CypherType)
+    label.pack()
+
+    button1 = tkinter.Button(root, text = "1", width = 10, height = 2)
+    button1.pack(side = tkinter.TOP)
+    button2 = tkinter.Button(root, text = "2", width = 10, height = 2)
+    button2.pack(side = tkinter.TOP)
 
 
 def get_encryption_type():
-    encryptionType = input("Enter a cipher option number: ")
-    while ((encryptionType.isdecimal() is False)
-        or (int(encryptionType) < 0 or int(encryptionType) > 5)):
-            print("Invalid input.")
-            encryptionType = input("Enter a cipher option number: ").strip()
-    return int(encryptionType)
+    label = tkinter.Label(topframe, text='Cipher Options')
+    label.pack()
+
+    button1 = tkinter.Button(root, text = "1: Caesar", width = 10, height = 2)
+    button1.pack(side = tkinter.TOP)
+    button2 = tkinter.Button(root, text = "2: Vigenere", width = 10, height = 2)
+    button2.pack(side = tkinter.TOP)
+    button3 = tkinter.Button(root, text = "3: Enigma", width = 10, height = 2)
+    button3.pack(side = tkinter.TOP)
+    button4 = tkinter.Button(root, text = "4: AES", width = 10, height = 2)
+    button4.pack(side = tkinter.TOP)
+    button5 = tkinter.Button(root, text = "5: Playfair", width = 10, height = 2)
+    button5.pack(side = tkinter.TOP)
 
 
-if __name__ == '__main__':
-    flag =  True
-    while flag:
-        # asks user for decryption or encryption
-        CypherType = get_cypher_type()
-        # FOR ENCRYPTION
-        if CypherType == 1:
-            TextType = get_text_type()
-            if TextType == 1:
-                filename = input('Plaintext filename: ')
-                text = Readtxt.readtxt(filename)
-            else:
-                text = input('\nInput the message you want to encrypt: ')
-        # FOR DECRYPTION
-        elif CypherType == 2:
-            TextType = get_text_type()
-            if TextType == 1:
-                filename = input('Plaintext filename: ')
-                text = Readtxt.readtxt(filename)
-            else:
-                text = input('\nInput the message you want to decrypt: ')
+def cont():
+    label = tkinter.Label(topframe, text='Continue?')
+    label.pack()
 
-        # Clean up text
-        text = text.replace('\n', ' ').lower()
-        whitelist = 'abcdefghijklmnopqrstuvwxyz '
-        text = ''.join(filter(whitelist.__contains__, text))
+    button1 = tkinter.Button(root, text = "Yes", width = 5, height = 2)
+    button1.pack()
+    button2 = tkinter.Button(root, text = "No", width = 5, height = 2)
+    button2.pack()
 
-        # picking the encryption method
-        encryption_dict = {1: 'Caesar', 2: 'Vigenere', 3: 'Enigma',
-                           4: 'AES', 5: 'playfair'}
-        print('\nCipher Options')
-        for k, v in encryption_dict.items():
-            print(f'  {k}: {v}')
 
-        encryptionType = get_encryption_type()
-        encryptionTypeVal = importlib.import_module(encryption_dict[encryptionType])
-        if CypherType == 1:
-            output = encryptionTypeVal.encrypt(text)
-        else:
-            output = encryptionTypeVal.decrypt(text)
+# get_text_type()
+# get_cypher_type()
+get_encryption_type()
+# cont()
 
-        pyperclip.copy(output)
-        print(f'\nOutput: {output}\nThe text has been copied to your clipboard :)')
-
-        cont = input('Want to continue? [Y/N]: ').upper().strip()
-        if cont != 'Y':
-            flag = False
+root.mainloop()
